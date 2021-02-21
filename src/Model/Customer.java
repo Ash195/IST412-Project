d76c9f85dfd415e681e86036428f76b53a4858e0
Kelvin <kelvin_trang@yahoo.com>
2021-02-21 11:59:14 -0500

    Added constructor to Customer

--- src/Model/Customer.java
+++ src/Model/Customer.java
@@ -1,5 +1,71 @@
 package Model;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class Customer {
     
+    private String username;
+    private String password;
+    private String email;
+    private String phoneNumber;
+    private int customerID;
+    
+    public Customer(String user, String pass, String em, String phone) {
+        
+        username = user;
+        password = pass;
+        email = em;
+        phoneNumber = phone;
+        customerID = 123456;
+    }
+
+    /**
+     * @return the username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * @param username the username to set
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * @return the customer's email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the customer ID
+     */
+    public int getCustomerID() {
+        return customerID;
+    }
 }
